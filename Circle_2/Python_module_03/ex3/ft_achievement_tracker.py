24baf5b2ce3419925a6cb1a529d1310ce64542a1
KuraiZ0 <pepeh1190@gmail.com>
2025-12-13 18:41:03 +0100

    Add files via upload

--- Circle_2/Python_module_03/ex3/ft_achievement_tracker.py
+++ Circle_2/Python_module_03/ex3/ft_achievement_tracker.py
@@ -10,30 +10,34 @@
 #                                                                             #
 # ****************************************************************************#
 
+
 class Player:
-    def __init__(self, name, level, speed, boss_slain, achievement={}):
+    def __init__(self, name, level, speed, boss_slain, achievement=None):
         self.name = name
         self.level = level
         self.speed = speed
-        self.achievement = achievement
         self.boss_slain = boss_slain
-    
+        if achievement is None:
+            self.achievement = set()
+        else:
+            self.achievement = achievement
+
     def check_achievement(player):
         if player.level >= 10:
-            player.achievement.add('level_10')
+            player.achievement.add("level_10")
         if player.speed >= 100:
-            player.achievement.add('speed_demon')
+            player.achievement.add("speed_demon")
         if player.name != "Bob":
-            player.achievement.add('treasure_hunter')
+            player.achievement.add("treasure_hunter")
         if player.boss_slain > 10:
-            player.achievement.add('boss_slayer')
+            player.achievement.add("boss_slayer")
 
 
 if __name__ == "__main__":
 
-    alice = Player("Alice", 17, 140, 3, {'first_kill'})
-    bob = Player("Bob", 170, 97, 62, {'collector', 'first_kill'})
-    charlie = Player("Charlie", 47, 140, 18, {'perfectionist'})
+    alice = Player("Alice", 17, 140, 3, {"first_kill"})
+    bob = Player("Bob", 170, 97, 62, {"collector", "first_kill"})
+    charlie = Player("Charlie", 47, 140, 18, {"perfectionist"})
 
     print("=== Achievement Tracker System ===")
 
@@ -47,23 +51,27 @@ if __name__ == "__main__":
 
     print("=== Achievement Analytics ===")
 
-    all_achievements = (
-        alice.achievement | bob.achievement | charlie.achievement)
+    all_achievements = (alice.achievement | bob.achievement
+                        | charlie.achievement)
     print(f"All unique achievements: {all_achievements}")
     print(f"Total unique achievements: {len(all_achievements)}")
     print()
 
     common = alice.achievement & bob.achievement & charlie.achievement
     print(f"Common to all players: {common}")
-    rare_achi = alice.achievement:difference(bob.achievement)) \
+    uniq_alice = alice.achievement - bob.achievement - charlie.achievement
+    uniq_bob = bob.achievement - alice.achievement - charlie.achievement
+    uniq_charlie = charlie.achievement - alice.achievement - bob.achievement
+
+    rare_achi = uniq_alice.union(uniq_alice, uniq_charlie)
     print(f"Rare achievements (1 player): {rare_achi}")
     print()
 
     ab_common = alice.achievement & bob.achievement
     print(f"{alice.name} vs {bob.name} common: {ab_common}")
-    """
-    je suis arrive a la dis toi ce quil manque cest le dernier paragraphe de
-    l'output oublie pas (.union() op: (|) & .intersection()
-    op: (&) & .difference() op: (-) et les operateurs
-    qui vont avec
-    """
+
+    alice_uniq = alice.achievement.difference(bob.achievement)
+    print(f"Alice unique: {alice_uniq}")
+
+    bob_uniq = bob.achievement.difference(alice.achievement)
+    print(f"Bob unique: {bob_uniq}")
