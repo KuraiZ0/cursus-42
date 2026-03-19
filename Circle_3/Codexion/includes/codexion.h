47e92480e9a0a6ec1f2a1bb4fe85e38289f90d2e
KuraiZ0 <pepeh1190@gmail.com>
2026-03-19 13:24:14 +0100

    1

---
+++ Circle_3/Codexion/includes/codexion.h
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialmani <ialmani@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 13:45:00 by ialmani           #+#    #+#             */
+/*   Updated: 2026/03/18 15:31:34 by ialmani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CODEXION_H
+# define CODEXION_H
+
+/*|=== INCLUDE PART ===|*/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+
+/*|=== STRUCT DECLARATION ===|*/
+typedef struct s_params {
+	int					nb_coders;
+	long				time_to_burnout;
+	long				time_to_compile;
+	long				time_to_debug;
+	long				time_to_refractor;
+	int					nb_compiles;
+	long				dongle_cd;
+	int					scheduler;
+	long				start_time;
+	int					stop;
+	pthread_mutex_t		log_mutex;
+	pthread_mutex_t		stop_mutex;
+}	t_params;
+
+typedef struct s_dongle {
+	int					available;
+	long				available_at;
+	pthread_mutex_t		t_mutex;
+	pthread_cond_t		t_cond;
+	// t_pqueue 			*waiters;
+}	t_dongle;
+
+typedef struct s_coder {
+	int					id;
+	t_params			*params;
+	t_dongle			*left;
+	t_dongle			*right;
+	long				last_compile;
+	int					compile_count;
+	pthread_mutex_t		state_mutex;
+}	t_coder;
+
+typedef struct s_sim
+{
+    t_params    params;
+    t_coder     *coders;
+    pthread_t   *threads;
+}   t_sim;
+
+/*|=== FUNCTION PROTOTYPE ===|*/
+int		parsing(int ac, char **av);
+int		valid_int(char *str);
+long	get_time_ms(void);
+void	log_event(t_sim *sim, int id, char *msg);
+
+
+
+#endif
---
+++ Circle_3/Codexion/src/log.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   log.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialmani <ialmani@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 14:01:29 by ialmani           #+#    #+#             */
+/*   Updated: 2026/03/18 15:42:12 by ialmani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+void    log_event(t_sim *sim, int id, char *msg)
+{
+    pthread_mutex_lock(&sim->params.log_mutex);
+    long time_now = get_time_ms();
+    long time_atm = time_now - sim->params.start_time;
+    printf("%ld %d %s\n", time_atm, id, msg);
+    pthread_mutex_unlock(&sim->params.log_mutex);
+}
